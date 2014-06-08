fb84192bf811b67a54bd30edd13f68ec9031145e
Jeff Sember <jpsember@gmail.com>
2014-06-07 22:58:55 -0700

    refactored TagSet to use move-to-front with singly linked list

--- RBuddy/src/js/rbuddy/TagSet.java
+++ RBuddy/src/js/rbuddy/TagSet.java
@@ -3,28 +3,26 @@ package js.rbuddy;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 import static js.basic.Tools.*;
 
 /**
- * To enforce queue limit, access tags by priority map: int => (name,int)
- * To display tags, access by name set: name
+ * To enforce queue limit, access tags by priority map: int => (name,int) To
+ * display tags, access by name set: name
  * 
  * Maybe (name,int) isn't necessary
  * 
  * @author jeff
- *
+ * 
  */
 public class TagSet {
 
 	public TagSet(int maxTags) {
 		this.maxTags = maxTags;
-		tagMap = new TreeMap<String, TagEntry>();
-		tagSet = new TreeSet<String>();
-		unimp("sort set by priority, and map by name");
-		unimp("rename tag -> name to disambiguate");
-		unimp("figure out MapEntry");
+		tailEntry = new TagEntry(null);
+		headEntry = tailEntry;
+
+		tagMap = new TreeMap<String, TagEntry>(String.CASE_INSENSITIVE_ORDER);
 	}
 
 	private static final int MAX_TAGS = 200;
@@ -34,77 +32,89 @@ public class TagSet {
 	}
 
 	public int size() {
-		return tagSet.size();
+		return size;
 	}
 
-	public boolean addTag(String tag) {
-		final boolean db = true;
-		if (db)
-			pr("addTag " + tag);
-		TagEntry entry = tagMap.remove(tag);
-		if (db)
-			pr(" removed tag, got entry " + entry);
+	public boolean addTag(String name) {
+		TagEntry entry = tagMap.get(name);
 		boolean tagExists = (entry != null);
-		if (!tagExists) {
-			entry = new TagEntry(tag, 0);
-		}
-		if (entry.priority <= this.priority) {
-			entry.priority = incrementPriority();
-		}
-		tagMap.put(tag, entry);
-		tagSet.add(tag);
-		if (db) pr("tagSet now:\n"+encode());
 
-		trimSize();
+		if (tagExists) {
+
+			// If tag is already at front, do nothing
+
+			if (entry != headEntry) {
+				// Move tag to the front
+
+				// Make this entry the container for its successor's value
+				TagEntry next = entry.next;
+
+				entry.name = next.name;
+				entry.next = next.next;
+				// Update this entry within the map, since its name has changed
+				tagMap.put(entry.name, entry);
+
+				// If the successor was the head entry, update it to this one
+				// instead
+				if (headEntry == next)
+					headEntry = entry;
+
+				entry = next;
+
+				entry.name = name;
+				entry.next = null;
+
+				// Make the old head point to us
+				headEntry.next = entry;
+				headEntry = entry;
+
+				// Update this entry within the map, since its name has changed
+				tagMap.put(name, entry);
+			}
+		} else {
+			if (size == maxTags) {
+				entry = tailEntry.next;
+
+				tagMap.remove(entry.name);
+				tailEntry.next = entry.next;
+
+				entry.next = null;
+				entry.name = name;
+			} else {
+				size++;
+				entry = new TagEntry(name);
+			}
+			headEntry.next = entry;
+			headEntry = entry;
+			// Update the entry associated with the name key, since the entry
+			// has changed
+			tagMap.put(name, entry);
+		}
+
+		if (db)
+			pr("tagSet now, in reverse priority order:\n" + encode()
+					+ "\n---------------------------");
+
 		return tagExists;
 	}
 
 	public Set<String> tags() {
-		return tagSet;
-	}
-
-	private void trimSize() {
-		final boolean db = true;
-		if (db) pr("trimSize, currently "+tagMap.size()+"/"+tagSet.size()+" (max "+maxTags+")");
-		while (tagMap.size() > maxTags) {
-			String tag = tagMap.lastKey();
-			if (db) pr(" attempting to remove "+tag);
-			tagMap.remove(tag);
-			tagSet.remove(tag);
-		}
-	}
-
-	private int incrementPriority() {
-		priority++;
-		return priority;
-	}
-
-	private static class TagEntry {
-		public TagEntry(String tag, int priority) {
-			this.tag = tag;
-			this.priority = priority;
-		}
-
-		@Override
-		public String toString() {
-			return "<" + tag + " #" + priority + ">";
-		}
-
-		private String tag;
-		private int priority;
+		return tagMap.keySet();
 	}
 
 	public String encode() {
 		StringBuilder sb = new StringBuilder();
-		for (Iterator<TagEntry> it = tagMap.values().iterator(); it.hasNext();) {
-			TagEntry tagEntry = it.next();
-			sb.append(tagEntry.tag);
+		TagEntry entry = tailEntry.next;
+		while (entry != null) {
+			sb.append(entry.name);
 			sb.append('\n');
+			entry = entry.next;
 		}
 		return sb.toString();
 	}
 
 	public static TagSet decode(String s) {
+		unimp("we can do this quicker by inserting objects ourselves");
 		String[] strs = s.split("\\n");
 		TagSet set = new TagSet();
 		for (int i = strs.length - 1; i >= 0; i--)
@@ -112,8 +122,43 @@ public class TagSet {
 		return set;
 	}
 
+	private static class TagEntry {
+		public TagEntry(String name) {
+			this.name = name;
+		}
+
+		// Reenable these lines if debugging is required
+		// @Override
+		// public String toString() {
+		// String s = "<" + name + " => ";
+		// if (next != null)
+		// s += next.name;
+		// else
+		// s += "null";
+		// s += ">";
+		// return s;
+		// }
+
+		private String name;
+		private TagEntry next;
+	}
+
+	/**
+	 * Provided for test purposes
+	 */
+	public void verifyInternalConsistency() {
+		Iterator<String> it = tagMap.keySet().iterator();
+		while (it.hasNext()) {
+			String name = it.next();
+			TagEntry entry = tagMap.get(name);
+			if (!name.equals(entry.name))
+				throw new IllegalStateException("map[" + name + "] points to "
+						+ entry);
+		}
+	}
+
+	private int size;
+	private TagEntry headEntry, tailEntry;
 	private int maxTags;
-	private int priority;
 	private TreeMap<String, TagEntry> tagMap;
-	private TreeSet<String> tagSet;
 }
--- RBuddyTest/src/js/rbuddy/test/TagSetTest.java
+++ RBuddyTest/src/js/rbuddy/test/TagSetTest.java
@@ -2,32 +2,23 @@ package js.rbuddy.test;
 
 import static org.junit.Assert.*;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.*;
 
-import js.basic.IOSnapshot;
 //import static org.junit.Assert.*;
-import js.rbuddy.JSDate;
-import js.rbuddy.Receipt;
 import js.rbuddy.TagSet;
 import static js.basic.Tools.*;
 
 public class TagSetTest extends js.testUtils.MyTest {
 
-	private static String[] script = { "zero", "alpha", "mary", "una",
-			"jofree", };
-	private static String[] script2 = { "q", "w", "e", "r", "t", "y", "u", "i",
-			"o", "p", };
-
 	private TagSet ts;
 
 	private static final int OUR_MAX_SIZE = 5;
-	
+
 	private TagSet build() {
 		if (ts == null)
 			ts = new TagSet(OUR_MAX_SIZE);
@@ -35,14 +26,25 @@ public class TagSetTest extends js.testUtils.MyTest {
 	}
 
 	private void addScript(String[] s) {
-		for (int i = 0; i<s.length; i++)
+		for (int i = 0; i < s.length; i++) {
 			ts.addTag(s[i]);
+			ts.verifyInternalConsistency();
+		}
 	}
-	
-	private String dump() {
-		return toString(build().tags());
+
+	private void addScript(String s) {
+		// For some reason, splitting the empty string produces one (empty)
+		// string instead of zero strings
+		String[] script = {};
+		if (s.length() > 0)
+			script = s.split("(?!^)");
+		// final boolean db = true;
+		if (db)
+			pr("addedScript '" + s + "', script length " + script.length);
+		assertEquals(script.length, s.length());
+		addScript(script);
 	}
-	
+
 	private static String toString(Set<String> set) {
 		StringBuilder sb = new StringBuilder();
 		for (Iterator it = set.iterator(); it.hasNext();) {
@@ -53,19 +55,145 @@ public class TagSetTest extends js.testUtils.MyTest {
 	}
 
 	@Test
-	public void testGetsTagsInAlphaOrder() {
+	public void testEmptySet() {
+		testScr("", "");
+	}
+
+	private void testScr(String s, String exp) {
 		build();
-		addScript(script);
-		assertStringsMatch("alpha jofree mary una zero ",
-				toString(ts.tags()));
+		addScript(s);
+		String q = toString(ts.tags());
+		q = q.replaceAll("\\s+", "");
+		assertStringsMatch(exp, q);
 	}
 
 	@Test
-	public void testBumpsLeastRecentlyUsed() {
-		build();
-		addScript(script2);
-		addScript(script2);
-		assertStringsMatch("p o i u y ",toString(ts.tags()));
-		assertEquals(ts.size(), OUR_MAX_SIZE);
+	public void testMoveToFrontNotNecessaryWithSize1() {
+		testScr("aaaaa", "a");
+	}
+
+	@Test
+	public void testMoveToFrontNotNecessaryWithSize2() {
+		testScr("abbb", "ab");
+	}
+
+	@Test
+	public void testMoveToFrontNotNecessaryWithSize3() {
+		testScr("abcccbbbaaabbbaaa", "abc");
+	}
+
+	@Test
+	public void testAdd2() {
+		testScr("baaaaa", "ab");
+	}
+
+	@Test
+	public void testAdd3() {
+		testScr("abcde", "abcde");
+	}
+
+	@Test
+	public void testAdd4() {
+		testScr("abcdef", "bcdef");
+	}
+
+	@Test
+	public void testAdd5() {
+		testScr("fedcba", "abcde");
+	}
+
+	@Test
+	public void testAlphaOrder() {
+		testScr("mzajhq", "ahjqz");
+	}
+
+	@Test
+	public void testAdd6() {
+		testScr("abcdeeeeeeeee", "abcde");
+	}
+
+	@Test
+	public void testAdd7() {
+		testScr("abcdeaaaaaaa", "abcde");
+	}
+
+	@Test
+	public void testMovingDifferentPositionsToFront() {
+		String s = "abcde";
+		for (int i = 0; i < s.length(); i++) {
+			int c = s.length() - 1 - i;
+			String s2 = s + s.substring(c, c + 1);
+			testScr(s2, s);
+		}
+	}
+
+	@Test
+	/**
+	 * Add random tags to a set with a large capacity, and simulate it using a simple
+	 * but slower data structure; verify that the contents match afterwards.
+	 */
+	public void testLargeTagSet() {
+
+		int maxTagSetSize = 5000;
+		TagSet set = new TagSet(maxTagSetSize);
+
+		// Construct a map of elements we expect to see in the tag set, mapped
+		// to the step number when they were 'born'
+		Map<String, Integer> nameToBirthdayMap = new HashMap();
+
+		// Construct a sorted set of tags, sorted by birthdate
+		Map<Integer, String> birthdayToNameMap = new HashMap();
+
+		// Keep track of the minimum birthday in our simulated data structure;
+		// we will look for this birthday in our data structure to find the one
+		// to toss out,
+		// and increment it until we do find one
+		int purgeBirthday = 0;
+
+		for (int stepNumber = 0; stepNumber < maxTagSetSize * 5; stepNumber++) {
+
+			// Choose a random name that will generate some collisions
+			// (simulating 'move to front')
+			String tagName = "__"
+					+ (random().nextInt(maxTagSetSize * 5) + 1000);
+
+			Integer prevBirthdayForThisName = nameToBirthdayMap.put(tagName,
+					stepNumber);
+			if (prevBirthdayForThisName != null) {
+				birthdayToNameMap.remove(prevBirthdayForThisName);
+			}
+			birthdayToNameMap.put(stepNumber, tagName);
+
+			set.addTag(tagName);
+
+			while (nameToBirthdayMap.size() > maxTagSetSize) {
+				while (true) {
+					assertTrue(purgeBirthday < stepNumber);
+					String removeName = birthdayToNameMap.get(purgeBirthday);
+
+					if (removeName == null) {
+						purgeBirthday++;
+						continue;
+					}
+
+					Object val = nameToBirthdayMap.remove(removeName);
+					assertTrue(val != null);
+					birthdayToNameMap.remove(purgeBirthday);
+					purgeBirthday++;
+					break;
+				}
+			}
+		}
+		
+		// We've added so many, and don't expect a ridiculous number of collisions; hence the
+		// tag set ought to be full
+		assertEquals(set.size(), maxTagSetSize); 
+		
+		// Verify that the contents are equal
+		for (Iterator<String> iter = nameToBirthdayMap.keySet().iterator(); iter
+				.hasNext();) {
+			String name = iter.next();
+			assertTrue(set.tags().contains(name));
+		}
 	}
 }
