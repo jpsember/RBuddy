c2733bb855fa76a1b581643fb071db76b4349ac6
Jeff Sember <jpsember@gmail.com>
2014-06-11 23:40:02 -0700

    Fix for issue #16, a new tokenizer that recognizes commas and periods

--- RBuddyApp/src/js/rbuddy/TagsEditText.java
+++ RBuddyApp/src/js/rbuddy/TagsEditText.java
@@ -11,7 +11,8 @@ public class TagsEditText extends MultiAutoCompleteTextView {
 		super(context);
 
 		setInputType(InputType.TYPE_CLASS_TEXT);
-		setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+		
+		setTokenizer(new OurTokenizer());
 		setKeyListener(TextKeyListener.getInstance(true,
 				TextKeyListener.Capitalize.NONE));
 
@@ -24,4 +25,55 @@ public class TagsEditText extends MultiAutoCompleteTextView {
 		setHint("Tags");
 	}
 
+	/**
+	 * Tokenizer that recognizes both periods and commas as delimeters
+	 */
+	private static class OurTokenizer implements Tokenizer {
+
+		private static boolean isDelim(char c) {
+			return c == ',' || c == '.';
+		}
+
+		private static boolean isWhitesp(char c) {
+			return c <= ' ';
+		}
+
+		@Override
+		public int findTokenStart(CharSequence text, int cursor) {
+			int i = cursor;
+			while (i > 0 && !isDelim(text.charAt(i - 1))) {
+				i--;
+			}
+			while (i < cursor && isWhitesp(text.charAt(i)))
+				i++;
+			return i;
+		}
+
+		@Override
+		public int findTokenEnd(CharSequence text, int cursor) {
+			int i = cursor;
+			int len = text.length();
+
+			while (i < len && !isDelim(text.charAt(i))) {
+				i++;
+			}
+			while (i > cursor && isWhitesp(text.charAt(i - 1)))
+				i--;
+			return i;
+		}
+
+		@Override
+		public CharSequence terminateToken(CharSequence text) {
+			int i = text.length();
+
+			while (i > 0 && isWhitesp(text.charAt(i - 1))) {
+				i--;
+			}
+			if (i > 0 && isDelim(text.charAt(i - 1))) {
+				return text;
+			}
+
+			return text + ", ";
+		}
+	}
 }
